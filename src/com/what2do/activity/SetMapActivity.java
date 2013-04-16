73bee3e7ea676b3030a171d01c324f8f58a82445
unknown <paul.e.crease@gmail.com>
2013-04-16 12:42:23 +0200

    update

--- src/com/what2do/activity/SetMapActivity.java
+++ src/com/what2do/activity/SetMapActivity.java
@@ -2,6 +2,12 @@ package com.what2do.activity;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.what2do.R;
 import com.what2do.R.layout;
 import com.what2do.R.menu;
@@ -11,14 +17,20 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class SetMapActivity extends FragmentActivity {
 
 	private GoogleMap mMap;
-
+	private Button setButton;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +39,24 @@ public class SetMapActivity extends FragmentActivity {
         mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.setmap)).getMap();
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         
-
+        setButton = (Button) findViewById(R.id.select_location);
+        
+        setButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	mMap.clear();
+            	
+            	float zoomLevel=mMap.getCameraPosition().zoom;
+            	Log.e("aaa",""+zoomLevel);
+            	LatLng mapCenter= mMap.getCameraPosition().target;
+            	 Marker marker = mMap.addMarker(new MarkerOptions()
+                 .position(mMap.getCameraPosition().target)
+                 .title("Search target")
+                 .draggable(true)
+                 .snippet("Hold finger on the marker and drag to move!"));
+            	 
+            	 marker.showInfoWindow();
+            }
+       });
 		
 		
     }
